a3ba759733aa0326cec44f3ada5f14c7f6c204eb
Dominik Malik <dominikm25@gmail.com>
2020-10-21 22:38:42 +0200

    start login form

---
+++ webapp/frontend/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './App.css';
+
+class App extends React.Component {
+  render(){
+  return (
+    <div className="app">
+      sadadst
+    </div>
+    
+  );
+  }
+}
+
+export default App;
